7609053116ce26b169381db74475f22b133686eb
zachey <63107653+zachey01@users.noreply.github.com>
2024-05-04 20:09:34 +0500

    Remove trash

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-document.onreadystatechange = function () {
-	if (document.readyState !== 'complete') {
-		document.querySelector('body').style.visibility = 'hidden';
-		document.querySelector('#spinner').style.visibility = 'visible';
-	} else {
-		document.querySelector('#spinner').style.display = 'none';
-		document.querySelector('body').style.visibility = 'visible';
-	}
-};
